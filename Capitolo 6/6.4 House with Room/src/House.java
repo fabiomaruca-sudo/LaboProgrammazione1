85c5f17140a00be65a0786e5ec426a31670abd21
fabiomaruca-sudo <fabio10maruca@gmail.com>
2025-12-03 12:31:15 +0100

    fine esericizio House

--- Capitolo 6/6.4 House with Room/src/House.java
+++ Capitolo 6/6.4 House with Room/src/House.java
@@ -12,15 +12,43 @@ public class House {
 
     //METODI
     //Questo metodo aggiungerà una stanza col metodo di composizione
-    public void aggiungiStanza(String nome,float superficieMq) {
+    public void aggiungiStanza(String nome, double superficieMq) {
+        if (nome.isEmpty()) {
+            throw new IllegalArgumentException("Nome invalido");
+        }
+        if (superficieMq <= 0) {
+            throw new IllegalArgumentException("SuperficieMq inferiore o uguale a 0");
+        }
         stanze.add(new Room(nome,superficieMq));
     }
 
     public void visualizzaStanze() {
         if (!stanze.isEmpty()) {
         for (int i = 0; i<stanze.size();i++) {
-                System.out.println("Camera " + i+1 + ": " + stanze.get(i).getNome() + " " + stanze.get(i).getSuperficieMq());
+                System.out.println("Camera " + (i+1) + ": " + stanze.get(i).getNome() + " " + stanze.get(i).getSuperficieMq());
             }
+        }else  {
+            System.out.println("La casa è vuota.");
         }
+    }
+
+    public double getSuperficieTotale() {
+        double superficie = 0.0;
+        for (Room stanza : stanze) {
+            superficie += stanza.getSuperficieMq();
+        }
+        return superficie;
+    }
+
+    public Room trovaStanza(String nome) {
+        for (Room stanza : stanze) {
+            if (stanza.getNome() == nome) {
+                return stanza;
+            }
+        } return null;
+    }
+
+    public void rimuoviStanza(String nome) {
+        stanze.remove(trovaStanza(nome));
     }
 }
--- Capitolo 6/6.4 House with Room/src/Main.java
+++ Capitolo 6/6.4 House with Room/src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        try {
+            House house = new House();
+            house.visualizzaStanze();
+            house.aggiungiStanza("cucina", 20);
+            house.aggiungiStanza("bagno", 5.5f);
+            house.aggiungiStanza("villa", 150);
 
+            house.visualizzaStanze();
+            System.out.println(house.getSuperficieTotale());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
